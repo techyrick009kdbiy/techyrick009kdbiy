196714040dd2a13129abecea0213ce31ccea5418
techyrick009kdbiy <youxin80816@163.com>
2024-06-11 22:47:33 +0800

    radio group init

---
+++ pages/radiogroup/index.js
@@ -0,0 +1,18 @@
+//index.js
+Page({
+  data: {
+    items: [
+      {value: 'USA', title: '美国'},
+      {value: 'CHN', title: '中国', checked: 'true'},
+      {value: 'BRA', title: '巴西'},
+      {value: 'JPN', title: '日本'},
+      {value: 'ENG', title: '英国'},
+    ],
+    country: ''
+  },
+  radioChange: function(e) {
+    this.setData({
+      country: e.detail.value
+    });
+  }
+})
