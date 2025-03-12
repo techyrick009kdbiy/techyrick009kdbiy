516a11a60eafa4651e034c4c3f5132cd749c3345
techyrick009kdbiy <youxin80816@163.com>
2025-03-12 16:29:05 +0800

    遍历例子项

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
 Page({
-  
+  data: {
+    items: [
+      {
+        title: 'Radio Group',
+        url: './radiogroup/index',
+      },
+      {
+        title: '弹窗',
+        url: './alert/index',
+      },
+      {
+        title: '城市切换',
+        url: './city/index',
+      },
+      {
+        title: '聊天界面',
+        url: './chat/index',
+      }
+    ]
+  }
 })
