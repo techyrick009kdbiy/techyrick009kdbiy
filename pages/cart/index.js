611ef3d44f64cea85ee94af98c1517b0e0015a39
techyrick009kdbiy <youxin80816@163.com>
2025-04-27 00:00:35 +0800

    bmob sdk 路径

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -1,4 +1,4 @@
-var Bmob = require('../../utils/bmob.js');
+var Bmob = require('./utils/bmob.js');
 Bmob.initialize("9a22a7cb3217e88cac2e81fe7e88c600", "025c1282712e387a62b2bcd5a970f93c");
 
 var that;
