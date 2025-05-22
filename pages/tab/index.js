fc64aa97dba3d3f9d49dba1115d096f6ea7c3058
techyrick009kdbiy <youxin80816@163.com>
2025-05-23 07:00:05 +0800

    底部栏tab加入图标

--- pages/tab/index.js
+++ pages/tab/index.js
@@ -2,7 +2,26 @@ var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
 
 Page({
     data: {
-        tabs: ["选项一", "选项二", "选项三"],
+				tabBar: {
+					"selectedColor": "#5DBCFF",
+					"normalColor": "#333",
+					"list": [{
+						"pagePath": "market/index/index",
+						"iconPath": "images/icon_home.png",
+						"selectedIconPath": "images/icon_home_selected.png",
+						"text": "首页"
+					}, {
+						"pagePath": "tools/index/index",
+						"iconPath": "images/icon_cate.png",
+						"selectedIconPath": "images/icon_cate_selected.png",
+						"text": "分类"
+					}, {
+						"pagePath": "viewing/index/index",
+						"iconPath": "images/icon_member.png",
+						"selectedIconPath": "images/icon_member_selected.png",
+						"text": "我"
+					}]
+				},
         activeIndex: 1,
         sliderOffset: 0,
         sliderLeft: 0
@@ -11,8 +30,8 @@ Page({
         wx.getSystemInfo({
             success: res => {
                 this.setData({
-                    sliderLeft: (res.windowWidth / this.data.tabs.length - sliderWidth) / 2,
-                    sliderOffset: res.windowWidth / this.data.tabs.length * this.data.activeIndex
+                    sliderLeft: (res.windowWidth / this.data.tabBar.list.length - sliderWidth) / 2,
+                    sliderOffset: res.windowWidth / this.data.tabBar.list.length * this.data.activeIndex
                 });
             }
         });
