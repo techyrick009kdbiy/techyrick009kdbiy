aa8956b265dfa835ff9de17a32069253e10cb49a
techyrick009kdbiy <youxin80816@163.com>
2025-11-20 09:35:06 +0800

    月份与年份切换

--- pages/calendar/index/index.js
+++ pages/calendar/index/index.js
@@ -7,6 +7,9 @@ Page({
     weekIndices: ['日', '一', '二', '三', '四', '五', '六']
   },
   onLoad() {
+    this.initData()
+  },
+  initData() {
     // 当天日期
     let today = moment()
     // 当年
@@ -18,8 +21,35 @@ Page({
       currentYear: currentYear,
       currentMonth: currentMonth
     })
+    this.generateData()
+  },
+  previousMonth() {
+    this.setData({
+      currentMonth: moment(`${this.data.currentYear}-${this.data.currentMonth}-01`).subtract(1, 'months').format('MM')
+    })
+    this.generateData()
+  },
+  nextMonth() {
+    this.setData({
+      currentMonth: moment(`${this.data.currentYear}-${this.data.currentMonth}-01`).add(1, 'months').format('MM')
+    })
+    this.generateData()
+  },
+  previousYear() {
+    this.setData({
+      currentYear: moment(`${this.data.currentYear}-${this.data.currentMonth}-01`).subtract(1, 'years').format('YYYY')
+    })
+    this.generateData()
+  },
+  nextYear() {
+    this.setData({
+      currentYear: moment(`${this.data.currentYear}-${this.data.currentMonth}-01`).add(1, 'years').format('YYYY')
+    })
+    this.generateData()
+  },
+  generateData() {
     // 本月开始日期
-    let startDayOfCurrentMonth = `${currentYear}-${currentMonth}-01`
+    let startDayOfCurrentMonth = `${this.data.currentYear}-${this.data.currentMonth}-01`
     // 本月结束日期
     let endDayOfCurrentMonth = moment(startDayOfCurrentMonth).add(1, 'month').format('YYYY-MM-DD')
     // 本月共几天
@@ -35,8 +65,8 @@ Page({
     // 换算农历
     let lunarArray = daysArray.map(item => {
       if (item != '') {
-        const lunar2solarData = solarLunar.solar2lunar(currentYear, currentMonth, item); // 输入的日子为农历
-        console.log(lunar2solarData)
+        const lunar2solarData = solarLunar.solar2lunar(this.data.currentYear, this.data.currentMonth, item); // 输入的日子为农历
+        // console.log(lunar2solarData)
         return lunar2solarData.dayCn
       }
       return item
