fd0651e585df04fba764aeefcb9bc66df7005318
techyrick009kdbiy <youxin80816@163.com>
2026-02-26 21:39:36 +0800

    按钮可见

--- pages/scrolltop/index/index.js
+++ pages/scrolltop/index/index.js
@@ -1,10 +1,24 @@
 Page({
   data: {
-    scrollHeight: wx.getSystemInfoSync().windowHeight
+    scrollHeight: wx.getSystemInfoSync().windowHeight,
+    visual: false
   },
-  goTop () {
+  goTop() {
     this.setData({
       scrollTop: 0
     })
+  },
+  scroll(e) {
+    let scrollTop = e.detail.scrollTop
+    // 如果大于一屏
+    if (scrollTop > this.data.scrollHeight / 2) {
+      this.setData({
+        visual: true
+      })
+    } else {
+      this.setData({
+        visual: false
+      })
+    }
   }
 })
