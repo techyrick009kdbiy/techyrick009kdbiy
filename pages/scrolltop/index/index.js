04dee31e929d88a14ac1967fc203e5581a0121f2
techyrick009kdbiy <youxin80816@163.com>
2026-03-09 09:38:36 +0800

    透明度动画

--- pages/scrolltop/index/index.js
+++ pages/scrolltop/index/index.js
@@ -1,7 +1,8 @@
 Page({
   data: {
     scrollHeight: wx.getSystemInfoSync().windowHeight,
-    visual: false
+    visual: false,
+    animation: ''
   },
   goTop() {
     this.setData({
@@ -10,15 +11,21 @@ Page({
   },
   scroll(e) {
     let scrollTop = e.detail.scrollTop
-    // 如果大于一屏
+    // 如果大于半屏
     if (scrollTop > this.data.scrollHeight / 2) {
       this.setData({
-        visual: true
+        visual: true,
+        animation: 'fadeIn'
       })
     } else {
       this.setData({
-        visual: false
+        animation: 'fadeOut'
       })
+      // setTimeout(() => {
+      //   this.setData({
+      //     visual: false
+      //   })
+      // }, 1000)
     }
   }
 })
